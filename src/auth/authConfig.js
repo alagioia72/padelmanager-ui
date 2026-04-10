74073ab9057602feeabc4159f4cb03b1d8348906
Alessandro La Gioia <alessandro.lagioia@bitron-ind.com>
2026-04-10 10:02:01 +0200

    Added VITE_AUTH_SCOPES secret

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -57,7 +57,7 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: [import.meta.env.VITE_AUTH_SCOPE],
+    scopes: [import.meta.env.VITE_AUTH_SCOPES],
 };
 
 /**
